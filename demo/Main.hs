9dfc7028198af5286e7af304822e47f5eb8df8d8
Joachim Breitner <mail@joachim-breitner.de>
2018-07-20 19:43:03 -0400

    Some style fixes

--- demo/Main.hs
+++ demo/Main.hs
@@ -6,58 +6,54 @@ import Reflex.Dom.FragmentShaderCanvas
 import qualified Data.Text as T
 
 main :: IO ()
-main = mainWidgetWithHead
-    (el "style" (text css) >> el "title" (text "Fragment Shader Demo")) $ mdo
-        inp <- divClass "left" $ do
-            inp <- textArea $ def
-               & textAreaConfig_initialValue .~ trivialFragmentShader
-            divClass "error" $ dynText (maybe "" id <$> dError)
-            return inp
+main = mainWidgetWithHead htmlHead $ mdo
+    inp <- divClass "left" $ do
+        inp <- textArea $ def
+           & textAreaConfig_initialValue .~ trivialFragmentShader
+        divClass "error" $ dynText (maybe "" id <$> dError)
+        return inp
 
-        dError <- divClass "right" $ fragmentShaderCanvas
-            -- Here we determine the resolution of the canvas
-            -- It would be desireable to do so synamically, based on the widget
-            -- size. But Reflex.Dom.Widget.Resize messes with the CSS layout.
-            (mconcat [ "width"  =: "1000" , "height" =: "1000" ])
-            (_textArea_value inp)
-        return ()
+    dError <- divClass "right" $ fragmentShaderCanvas
+        -- Here we determine the resolution of the canvas
+        -- It would be desireable to do so synamically, based on the widget
+        -- size. But Reflex.Dom.Widget.Resize messes with the CSS layout.
+        (mconcat [ "width"  =: "1000" , "height" =: "1000" ])
+        (_textArea_value inp)
+    return ()
+  where
+    htmlHead = do
+        el "style" (text css)
+        el "title" (text "Fragment Shader Demo")
 
 css :: T.Text
 css = T.unlines
-    [ "document {"
+    [ "html {"
     , "  margin: 0;"
-    , "  padding: 0;"
-    , "  width: 100vw;"
-    , "  height: 100vh;"
+    , "  height: 100%;"
     , "}"
     , "body {"
-    , "  margin: 0;"
-    , "  padding: 0;"
     , "  display: flex;"
-    , "  align-items: stretch;"
-    , "  width: 100vw;"
-    , "  height: 100vh;"
-    , "}"
-    , "textarea {"
-    , "  width: 50vw;"
-    , "  height: 50vh;"
-    , "}"
-    , ".error {"
-    , "  width: 100%;"
-    , "  text-align: left;"
-    , "  white-space:pre;"
-    , "  font-family:mono;"
-    , "  padding-top: 2em;"
-    , "  overflow: scrolll"
+    , "  height: 100%;"
     , "}"
     , ".left {"
-    , "  width:50%;"
-    , "  padding:2em;"
+    , "  flex:1 1 0;"
+    , "  display:flex;"
+    , "  flex-direction:column;"
+    , "  padding:1em;"
     , "}"
     , ".right {"
+    , "  flex:1 1 0;"
+    , "}"
+    , "textarea {"
+    , "  resize:vertical;"
+    , "  height:50%;"
+    , "}"
+    , ".error {"
+    , "  margin-top:1em;"
+    , "  text-align: left;"
+    , "  font-family:mono;"
     , "  width:100%;"
-    , "  padding: 10%;"
-    , "  display: flex;"
+    , "  overflow: auto;"
     , "}"
     , "canvas {"
     , "  height:100%;"
